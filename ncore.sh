5a07a108f5baa16b953398c8e8fa691a261ec422
John Sanabria <john.sanabria@correounivalle.edu.co>
2018-03-17 08:31:09 -0500

    Modificacion ncore.sh

--- ncore.sh
+++ ncore.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-NCORE="dialog --backtitle \"Num CPU\" --radiolist \"Seleccion num cpus:\" 10 40 4"
-for i in $(seq 1 1 2); do
-NCORE="${NCORE} ${i} ${i} off"
+#
+# Este script permite la seleccion del numero de nucleos que quiero asignar a
+# mi maquina virtual
+#
+NUMCORES=$(cat /proc/cpuinfo | grep Processor | wc -l) 
+NUMCORES=$(( NUMCORES/2 ))
+NCORE="dialog --backtitle \"Num CPU\" --radiolist \"Seleccion num cpus:\" 10 40 4" 
+for i in $(seq 1 1 ${NUMCORES}); do
+	NCORE="${NCORE} ${i} ${i} off"
 done
 NCORE="${NCORE} 2> salida"
 eval ${NCORE}
