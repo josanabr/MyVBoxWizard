a9df830073be2b9c66ad0bbcd8b16f64770d6312
John Sanabria <john.sanabria@correounivalle.edu.co>
2018-03-17 08:49:02 -0500

    ncore.sh

--- ncore.sh
+++ ncore.sh
@@ -4,7 +4,8 @@
 # mi maquina virtual
 #
 NUMCORES=$(cat /proc/cpuinfo | grep Processor | wc -l) 
-NUMCORES=$(( NUMCORES/2 ))
+NUMTHREAD=$(cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $4}')
+NUMCORES=$(( NUMCORES*NUMTHREAD/2 ))
 NCORE="dialog --backtitle \"Num CPU\" --radiolist \"Seleccion num cpus:\" 10 40 4 1 1 on" 
 for i in $(seq 2 1 ${NUMCORES}); do
 	NCORE="${NCORE} ${i} ${i} off"
